2cc023d8b47a28d01e39cfff9fe1adbf27d5aae1
Azpriw911 <fnmn224@gmail.com>
2025-05-22 15:00:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+kimport streamlit as st
 
 st.title("Kaoh911")
 st.write(
@@ -6,3 +6,11 @@ st.write(
 )
 
 st.image("view/Screenshot_20250207-160804.jpg")
+
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number-input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
